01191fc67db11c353ddbe1d9f8e6d52c495f5668
박상혁 <pshsh910@gmail.com>
2026-09-09 23:09:52 +0900

    feat: 한국 표준시 기준 Clock 구성

--- src/main/java/com/fmi/config/TimeConfig.java
+++ src/main/java/com/fmi/config/TimeConfig.java
@@ -1,14 +1,17 @@
 package com.fmi.config;
 
 import java.time.Clock;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TimeConfig {
 
+    private static final ZoneId SEOUL_ZONE = ZoneId.of("Asia/Seoul");
+
     @Bean
     public Clock clock() {
-        return Clock.systemDefaultZone();
+        return Clock.system(SEOUL_ZONE);
     }
 }
